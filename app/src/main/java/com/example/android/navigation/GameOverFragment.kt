04548cb08361ce8ef8be3ad050e28e133260f461
vaibhavjhingran <vaibhavjhingran@gmail.com>
2020-08-27 18:22:14 +0200

    Add safeargs to app

--- app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -33,7 +33,7 @@ class GameOverFragment : Fragment() {
         val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_over, container, false)
         binding.tryAgainButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_gameOverFragment2_pop_including_gameFragment2)
+            view.findNavController().navigate(GameOverFragmentDirections.actionGameOverFragment2PopIncludingGameFragment2())
         }
         return binding.root
     }
